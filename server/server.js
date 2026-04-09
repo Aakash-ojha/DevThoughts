a30d7c2e82e224f7c80f0575d1902c350182448f
Aakash-ojha <aakashojha000@gmail.com>
2026-04-09 12:44:36 +0545

    add 0.0.0.0 binidng port for porduction

--- server/server.js
+++ server/server.js
@@ -2,17 +2,21 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import app from "./app.js";
 
-const PORT = process.env.PORT || 7777;
+const PORT = process.env.PORT || 8000;
 
 const DB = process.env.MONGO_URI.replace(
   "<db_password>",
   process.env.DB_PASSWORD,
 );
 
-mongoose.connect(DB).then(() => {
-  app.listen(PORT, () => {
-    console.log(" DB connection sucessful");
-    console.log("Server running on port " + PORT);
-    // console.log("Environment: " + process.env.NODE_ENV);
+mongoose
+  .connect(DB)
+  .then(() => {
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log("DB connection successful");
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(" ❌ DB connection error:", err);
   });
-});
